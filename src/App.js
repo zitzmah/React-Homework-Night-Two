5d569ad422e8678929ca360e49f11fff095de3a2
HZ <haleyzitzmann@gmail.com>
2024-01-28 11:00:44 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  const movies = [
+    {
+      name: "Inception",
+      year: "2010",
+      rating: "PG-13",
+      image: "https://m.media-amazon.com/images/I/714roYD1PYL._AC_UF894,1000_QL80_.jpg"
+    },
+    {
+      name: "The Shawshank Redemption",
+      year: "1994",
+      rating: "R",
+      image: "https://m.media-amazon.com/images/I/71715eBi1sL._AC_UF894,1000_QL80_.jpg"
+    },
+    {
+      name: "The Godfather",
+      year: "1972",
+      rating: "R",
+      image: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+    },
+    {
+      name: "Pulp Fiction",
+      year: "1994",
+      rating: "R",
+      image: "https://m.media-amazon.com/images/I/81p2ZUI-+RL._AC_UF894,1000_QL80_.jpg"
+    },
+    {
+      name: "The Dark Knight",
+      year: "2008",
+      rating: "PG-13",
+      image: "https://i5.walmartimages.com/seo/Dark-Knight-Movie-Batman-Movie-Laminated-Framed-Poster-24-x-36_aa45424c-368f-4596-8520-1a32415906ea.fe3d779cc6b1d55e01dd2da08d87bb75.jpeg"
+    }
+  ];
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
 
 export default App;
+
